92d4bb4b714c1d897598e89a4a06331318b6d4b6
Joshua Prince <87655528+joshuajpr@users.noreply.github.com>
2026-02-03 12:40:12 -0700

    Update german_credit.py

--- german_credit.py
+++ german_credit.py
@@ -102,7 +102,7 @@ def metrics(data):
 
     output_metrics_df = disparity_metrics_df # or absolute_metrics_df
     try:
-        print(json.loads(output_metrics_df.to_dict(orient="records"))
+        print(json.loads(output_metrics_df.to_dict(orient="records")))
     except:
         print(output_metrics_df)
         output_metrics_df.to_csv("out.csv", index=False)
