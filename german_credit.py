a30bc1f0d1ed57fca500ee157c6b67dd05263d79
Joshua Prince <87655528+joshuajpr@users.noreply.github.com>
2026-02-03 13:46:08 -0700

    replace nans

--- german_credit.py
+++ german_credit.py
@@ -44,6 +44,30 @@ def action(data):
     # MOC expects the action function to be a *yield* function
     yield data.to_dict(orient="records")
 
+def fix_numpy_nans_and_infs_in_dict(values: dict) -> dict:
+    """A function to change all numpy.nan and numpy.inf values in a flat dictionary to python Nones.
+
+    Args:
+        values (dict): Input dict to fix.
+        test_name (str):  Name of test that's calling this function.
+
+    Returns:
+        dict: Fixed dict.
+    """
+
+    for key, val in values.items():
+        # If value is numeric (not None), check for numpy.nan and numpy.inf
+        # If True, change to None, else keep unchanged
+        if val is not None:
+            try:  # Some values are not numeric
+                if np.isnan(val):
+                    values[key] = None
+                elif np.isinf(val):
+                    values[key] = None
+            except TypeError:
+                pass
+
+    return values
 
 # modelop.metrics
 def metrics(data):
@@ -101,6 +125,8 @@ def metrics(data):
     """
 
     output_metrics_df = disparity_metrics_df # or absolute_metrics_df
-
+    output_metrics = output_metrics_df.to_dict(orient="records")
+    for record in output_metrics:
+        fix_numpy_nans_and_infs_in_dict(record)
     # Output a JSON object of calculated metrics
-    yield output_metrics_df.to_dict(orient="records")
+    yield output_metrics
